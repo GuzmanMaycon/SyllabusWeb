9f427cff27f9fb4b4284ed9f41d530f7ea727521
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2016-11-01 23:37:00 -0500

    javadoc agregado y bean para syllabus controller

--- src/main/java/com/junior/controller/SyllabusController.java
+++ src/main/java/com/junior/controller/SyllabusController.java
@@ -1,29 +1,50 @@
 package com.junior.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.junior.dao.component.AsignaturaAperturadaDao;
 import com.junior.dao.design.IAsignaturaAperturadaDao;
+
+/**
+ *  Controlador para el manejo de syllabus
+ *  @author Junior Claudio
+ *  @version 1.1, 01/11/16
+ *  @see com.junior.dao.component.AsignaturaAperturadaDao#obtenerNombreDeAsignaturaPorId obtenerNombreDeAsignatura
+ */
 
 @Controller
 @RequestMapping("/asignatura/{asignaturaAperturadaId}/syllabus")
 public class SyllabusController {
 
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public String create(ModelMap map, @PathVariable(value="asignaturaAperturadaId") Integer id)
+    @Autowired
+    private IAsignaturaAperturadaDao asignaturaAperturadaDao; // Dao para asignaturas aperturadas
+
+    public void setAsignaturaAperturadaDao(IAsignaturaAperturadaDao asignaturaAperturadaDao)
     {
-        IAsignaturaAperturadaDao asignaturaAperturadaDao = new AsignaturaAperturadaDao();
-        String nombreCurso = asignaturaAperturadaDao.obtenerNombreDeAsignaturaPorId(id);
-        if (nombreCurso == null) {
+        this.asignaturaAperturadaDao = asignaturaAperturadaDao;
+    }
+
+    /**
+     * Controlar si se muestra la vista para registrar syllabus o se redirige a
+     * la ventana anterior con un mensaje de error
+     * @param model Modelo para usar data del controlador en la vista
+     * @param id Id de la Asignatura Aperturada
+     * @return la vista de registrar si el id es valido sino redireccionar
+     */
+    @RequestMapping(value = "/registrar", method = RequestMethod.GET)
+    public String create(ModelMap model, @PathVariable(value="asignaturaAperturadaId") Integer id)
+    {
+        String nombreAsignatura = this.asignaturaAperturadaDao.obtenerNombreDeAsignaturaPorId(id);
+        if (nombreAsignatura == null) {
             //Redirigir indicando que el id de la asignatura aperturada no es correcta
             //To-do
         }
-        map.addAttribute("course_name", nombreCurso);
+        model.addAttribute("nombreAsignatura", nombreAsignatura);
 
-        return "syllabus/create";
+        return "syllabus/registrar";
     }
 }
