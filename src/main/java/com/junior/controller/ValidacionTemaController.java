02b48da65871c5c674bdcd4c376422acd103c2f0
Junior Claudio <juniorclaudiozavaleta@gmail.com>
2016-11-20 02:11:31 -0500

    con el usuario logueado obtener los temas para validar

--- src/main/java/com/junior/controller/ValidacionTemaController.java
+++ src/main/java/com/junior/controller/ValidacionTemaController.java
@@ -1,6 +1,6 @@
 package com.junior.controller;
 
-import java.util.ArrayList;
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +10,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.junior.dao.design.IAsignaturaAperturadaDao;
 import com.junior.dao.design.ITemaDao;
+import com.junior.dao.design.IUsuarioDao;
 import com.junior.to.Tema;
+import com.junior.to.Usuario;
 
 @Controller
 @RequestMapping("/grupo/{grupoId}")
 public class ValidacionTemaController {
-	
-	@Autowired
+
+    @Autowired
     private ITemaDao temaDao; // Dao para asignaturas aperturadas
+
+    @Autowired
+    public IUsuarioDao usuarioDao;
 
     /**
      * Asignar el dao para temas del syllabus
@@ -30,26 +34,27 @@ public class ValidacionTemaController {
         this.temaDao = temaDao;
     }
 
+    public void setUsuarioDao(IUsuarioDao usuarioDao)
+    {
+        this.usuarioDao = usuarioDao;
+    }
+
     @RequestMapping(value = "/valida", method = RequestMethod.GET)
     public String create(ModelMap map,
-        @PathVariable(value="grupoId") Integer grupoId)
+        @PathVariable(value="grupoId") Integer grupoId,
+        Principal principal)
     {
         String nombreAsignatura = "";
-        
+
         //IDUsuario del alumno que ingresa a la página
-        Integer alumnoId = 1;
-        
+        Usuario usuario = this.usuarioDao.obtenerUsuario(principal.getName());
+
         map.addAttribute("nombreAsignatura", nombreAsignatura);
 
         //Prueba del DAO pasando como parametro el alumnoId para validar su asistencia con los temas a validar
-        List<Tema> temas2 = this.temaDao.obtenerAvancePorValidar(grupoId,alumnoId);
-        
-        List<Tema> temas = new ArrayList<Tema>();
-        temas.add((new Tema(1, "Intro a la programacion", 1, 1,2,1)));
-        temas.add((new Tema(2, "Variables", 1, 1,2,1)));
-        temas.add((new Tema(3, "Arreglos", 1, 1,2,1)));
-        
-        map.addAttribute("temaList", temas);
+        List<Tema> temas = this.temaDao.obtenerAvancePorValidar(grupoId, usuario.getId());
+
+        map.addAttribute("temas", temas);
 
         return "temas/registrar_avance";
     }
